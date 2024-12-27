939a713e908a7afb56e0cf982e4c757011979e5c
Edwin Cardenas <eacardenase@gmail.com>
2024-12-27 15:55:37 -0500

    feat: check variables scopes

--- TurkeyTimer/main.c
+++ TurkeyTimer/main.c
@@ -11,6 +11,14 @@ void showCookTimeForTurkey(int pounds)
 {
     int necessaryMinutes = 15 + 15 * pounds;
     printf("Cook for %d minutes.\n", necessaryMinutes);
+    
+    if (necessaryMinutes > 120) {
+        int halfway = necessaryMinutes / 2;
+        
+        printf("Rotate after %d of the %d minutes.\n", halfway, necessaryMinutes);
+    }
+    
+//    printf("Rotate after %d of the %d minutes.\n", halfway, necessaryMinutes); // compilation error
 }
 
 int main(int argc, const char * argv[]) {
